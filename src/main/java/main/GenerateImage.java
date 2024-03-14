27e606d03b0e40c8fe0222ee2194e8b59d72e9fd
D028650 <stefan.gernhardt@sap.com>
2024-03-14 16:19:49 +0100

    generic finsished

--- src/main/java/main/GenerateImage.java
+++ src/main/java/main/GenerateImage.java
@@ -13,33 +13,47 @@ import java.util.Random;
 import static org.deeplearning.MnistSimple.IMG_SIZE;
 
 public class GenerateImage {
+    private static final int MEAN_LOOP = 50;
+
     private int mode = 0;
+
     private MnistSimple mnistSimple = null;
+
     private MnistSimple mnistSimple196 = null;
+
     Generator generator = null;
+
     Discriminator discriminator = null;
 
     public final static int N_GEN = 100;
+
     public final static int ELITE_N_GEN = 40;
+
     INDArray geneticINDArray = null;
+
     INDArray geneticMeanINDArray = null;
+    INDArray saveGeneticMeanINDArray = null;
+
     INDArray geneticSTDINDArray = null;
-    double [] geneticScore = null;
+    INDArray saveGeneticSTDINDArray = null;
+
+    double[] geneticScore = null;
+
     INDArray eliteGeneticINDArray = null;
 
     INDArray imageINDArray = null;
+
     private int digit = 0;
 
     private boolean goodImageCreated = false;
-    private int iteration = 0;
 
+    private int iteration = 0;
 
     public GenerateImage() {
         iteration = 0;
         goodImageCreated = false;
 
         imageINDArray = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
-
     }
 
     private void setUpGenerateSimple() {
@@ -91,9 +105,11 @@ public class GenerateImage {
         geneticINDArray = Nd4j.zeros(N_GEN, IMG_SIZE * IMG_SIZE);
 
         geneticMeanINDArray = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
+        saveGeneticMeanINDArray = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
         Util.setIndArrayToConstant(geneticMeanINDArray, 0.5);
 
         geneticSTDINDArray = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
+        saveGeneticSTDINDArray = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
         Util.setIndArrayToConstant(geneticSTDINDArray, 0.5);
 
         eliteGeneticINDArray = Nd4j.zeros(ELITE_N_GEN, IMG_SIZE * IMG_SIZE);
@@ -113,51 +129,49 @@ public class GenerateImage {
     }
 
     void setDigit(int d) {
-        digit =d;
+        digit = d;
     }
 
     public void generate(int digit) {
         setDigit(digit);
 
-        INDArray zeroInput = Nd4j.zeros(1, IMG_SIZE*IMG_SIZE);
+        INDArray zeroInput = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
         double similarity = mnistSimple.askModel(zeroInput, digit);
         System.out.println("similarity zero input to digit " + digit + " is " + similarity);
 
         Random random = new Random();
-        INDArray randomInput = Nd4j.zeros(1, IMG_SIZE*IMG_SIZE);
-        for(int i=0; i<IMG_SIZE*IMG_SIZE; i++) {
+        INDArray randomInput = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
+        for (int i = 0; i < IMG_SIZE * IMG_SIZE; i++) {
             randomInput.put(0, i, random.nextDouble());
         }
         similarity = mnistSimple.askModel(randomInput, digit);
         System.out.println("similarity random input to digit " + digit + " is " + similarity);
 
-
-        INDArray trainImage5Input = Nd4j.zeros(1, IMG_SIZE*IMG_SIZE);
+        INDArray trainImage5Input = Nd4j.zeros(1, IMG_SIZE * IMG_SIZE);
         for (int i = 0; i < IMG_SIZE * IMG_SIZE; i++) {
-            trainImage5Input.put(0, i, mnistSimple.getTrainingImage(0).getValue(i/IMG_SIZE, i%IMG_SIZE));
+            trainImage5Input.put(0, i, mnistSimple.getTrainingImage(0).getValue(i / IMG_SIZE, i % IMG_SIZE));
         }
         similarity = mnistSimple.askModel(trainImage5Input, digit);
         System.out.println("similarity trainingImage5 as input to digit " + digit + " is " + similarity);
-
 
         UI ui = new UI(true);
         ui.mainLoop(this);
     }
 
     public double getGrayValue(int row, int col) {
-        return imageINDArray.getDouble(0, row*IMG_SIZE+col);
+        return imageINDArray.getDouble(0, row * IMG_SIZE + col);
     }
 
     public void findBetterImage() {
-        if(mode == 1) {
+        if (mode == 1) {
             findBetterImageSimple();
             return;
         }
-        if(mode == 2) {
+        if (mode == 2) {
             findBetterImageForDiscriminatorAndGenerator();
             return;
         }
-        if(mode == 3) {
+        if (mode == 3) {
             findBetterImageGeneticAlgo();
             return;
         }
@@ -167,7 +181,7 @@ public class GenerateImage {
     }
 
     public void findBetterImageSimple() {
-        if(goodImageCreated) return;
+        if (goodImageCreated) return;
 
         iteration++;
         System.out.println("iteration: " + iteration);
@@ -177,7 +191,7 @@ public class GenerateImage {
         double minSimilarityBefore = Math.min(similarityBefore, similarity196Before);
 
         Random random = new Random();
-        int index = random.nextInt(IMG_SIZE*IMG_SIZE);
+        int index = random.nextInt(IMG_SIZE * IMG_SIZE);
 
         double beforeValue = imageINDArray.getDouble(0, index);
         imageINDArray.put(0, index, random.nextDouble());
@@ -186,38 +200,40 @@ public class GenerateImage {
         double similarity196After = mnistSimple196.askModel(imageINDArray, digit);
         double minSimilarityAfter = Math.min(similarityAfter, similarity196After);
 
-        if(minSimilarityAfter <= minSimilarityBefore) {
+        if (minSimilarityAfter <= minSimilarityBefore) {
             imageINDArray.put(0, index, beforeValue);
             System.out.println("similarity:    " + similarityBefore);
-        }
-        else {
+        } else {
             System.out.println("similarity:    " + similarityAfter);
         }
 
         double similarity196 = mnistSimple196.askModel(imageINDArray, digit);
         System.out.println("similarity196: " + similarity196);
 
-        if(similarityAfter >= 0.999) {
+        if (similarityAfter >= 0.999) {
             this.mnistSimple.printImage(0);
             this.mnistSimple.printImage(imageINDArray);
             goodImageCreated = true;
         }
-
     }
 
     public void findBetterImageForDiscriminatorAndGenerator() {
         imageINDArray = generator.askModel();
-
     }
 
     private void findBetterImageGeneticAlgo() {
-        if(iteration >= 2500) return;
-
         iteration++;
+        if (iteration >= 2500) return;
+
+        double meanScoreBefore = calculateMeanScore();
+
+        System.out.println("iteration: " + iteration + "  meanScoreBefore: " + meanScoreBefore);
+        if (meanScoreBefore > 0.92) return;
+
         Util.setIndArrayWithGaussDistribution(geneticINDArray, geneticMeanINDArray, geneticSTDINDArray, 0.0, 1.0);
 
-        for(int i=0; i<N_GEN; i++) {
-            geneticScore[i] = mnistSimple.askModel(geneticINDArray.getRow(i).reshape(1, IMG_SIZE*IMG_SIZE), digit);
+        for (int i = 0; i < N_GEN; i++) {
+            geneticScore[i] = mnistSimple.askModel(geneticINDArray.getRow(i).reshape(1, IMG_SIZE * IMG_SIZE), digit);
             // System.out.println("score: " + i + " " + geneticScore[i]);
         }
 
@@ -227,15 +243,12 @@ public class GenerateImage {
         // System.out.println("elite array with rowcol");
         // printOutRowCol(eliteGeneticINDArray);
 
-        double[] forTest = { 0.036745186895132065, 0.05668370798230171, 0.06619622558355331, 0.0216363538056612, 0.07507462054491043, 0.0508420467376709, 0.1080874502658844, 0.057289399206638336, 0.044289011508226395, 0.07861603051424026 }; //!
-
         int eliteRow = 0;
-        for(int i=0; i<N_GEN; i++) {
-            //! if(isEliteScore(forTest[i], forTest)) {
-            if(isEliteScore(geneticScore[i], geneticScore)) {
-                if(eliteRow < ELITE_N_GEN) {
+        for (int i = 0; i < N_GEN; i++) {
+            if (isEliteScore(geneticScore[i], geneticScore)) {
+                if (eliteRow < ELITE_N_GEN) {
                     // eliteGeneticINDArray.putRow(eliteRow, geneticINDArray.getRow(i));
-                    for(int c=0; c<geneticINDArray.columns(); c++) {
+                    for (int c = 0; c < geneticINDArray.columns(); c++) {
                         double value = geneticINDArray.getDouble(i, c);
                         eliteGeneticINDArray.put(eliteRow, c, value);
                     }
@@ -253,26 +266,44 @@ public class GenerateImage {
         // System.out.println("geneticSTDINDArray");
         // printOutRowCol(geneticSTDINDArray);
 
+        copyIndArrayElementByElement(geneticMeanINDArray, saveGeneticMeanINDArray);
+        copyIndArrayElementByElement(geneticSTDINDArray, saveGeneticSTDINDArray);
+
         Util.computeMeanAndStd(eliteGeneticINDArray, geneticMeanINDArray, geneticSTDINDArray);
-        System.out.println("geneticMeanINDArray");
-        printOutRowCol(geneticMeanINDArray);
+        // System.out.println("geneticMeanINDArray");
+        // printOutRowCol(geneticMeanINDArray);
 
-        System.out.println("geneticSTDINDArray");
-        printOutRowCol(geneticSTDINDArray);
+        // System.out.println("geneticSTDINDArray");
+        // printOutRowCol(geneticSTDINDArray);
 
-        // Util.setIndArrayWithGaussDistribution(imageINDArray, 0.5, 0.5, 0.0, 1.0); //! pd
         Util.setIndArrayWithGaussDistribution(imageINDArray, geneticMeanINDArray, geneticSTDINDArray, 0.0, 1.0);
 
-        double score = mnistSimple.askModel(imageINDArray, digit);
-        System.out.println("iteration: " + iteration + "  score: " + score);
+        double meanScoreAfter1 = calculateMeanScore();
+        System.out.println("iteration: " + iteration + "  meanscore after1: " + meanScoreAfter1);
 
-        // if(iteration == 25) System.exit(1);
+        double meanScoreAfter2 = calculateMeanScore();
+        System.out.println("iteration: " + iteration + "  meanscore after2: " + meanScoreAfter2);
+
+        System.out.println("geneticMeanINDArray changed values");
+        printOutRowCol(geneticMeanINDArray);
+
+        if (meanScoreBefore > meanScoreAfter1) {
+            copyIndArrayElementByElement(saveGeneticMeanINDArray, geneticMeanINDArray);
+            copyIndArrayElementByElement(saveGeneticSTDINDArray, geneticSTDINDArray);
+
+            System.out.println("geneticMeanINDArray restored values");
+            printOutRowCol(geneticMeanINDArray);
+        }
+
+        double meanScoreAfter3 = calculateMeanScore();
+        System.out.println("iteration: " + iteration + "  meanscore after3: " + meanScoreAfter3);
+        System.out.println("----------------------------------------");
     }
 
     private boolean isEliteScore(double v, double[] geneticScore) {
         int rank = 1;
-        for(int i=0; i<N_GEN; i++) {
-            if(v < geneticScore[i]) rank++;
+        for (int i = 0; i < N_GEN; i++) {
+            if (v < geneticScore[i]) rank++;
         }
 
         boolean isElite = rank <= ELITE_N_GEN;
@@ -292,5 +323,23 @@ public class GenerateImage {
         }
     }
 
+    private double calculateMeanScore() {
+        double meanScore = 0;
+        for(int i = 0;i<MEAN_LOOP;i++) {
+            Util.setIndArrayWithGaussDistribution(imageINDArray, geneticMeanINDArray, geneticSTDINDArray, 0.0, 1.0);
+            double score = mnistSimple.askModel(imageINDArray, digit);
+            meanScore = meanScore + score;
+        }
+        return meanScore / MEAN_LOOP;
+    }
 
+
+    public void copyIndArrayElementByElement(INDArray a1, INDArray a2) {
+        for (int r = 0; r < a1.rows(); r++) {
+            for (int c = 0; c < a1.columns(); c++) {
+                double value = a1.getDouble(r, c);
+                a2.put(r, c, value);
+            }
+        }
+    }
 }
